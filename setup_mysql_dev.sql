ae3d5151a04c7525d6681ca3f7176e8fa134d7de
Eric Ogbekene <ericogbekene@icloud.com>
2024-02-21 14:35:24 -0800

    script to setup mysql database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- script to setup a mysql server
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' ;
+ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
