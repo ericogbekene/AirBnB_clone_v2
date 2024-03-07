f2a82c7bb782a39531ed5ccb68a0b7fd280fd376
Eric Ogbekene <ericogbekene@icloud.com>
2024-03-07 13:22:51 -0800

    pycodestyle validatio for archive script

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,6 +7,7 @@ import datetime
 from fabric.api import *
 import os
 
+
 def do_pack():
     """
     function to pack a .tgz archive
@@ -15,13 +16,15 @@ def do_pack():
     time = datetime.datetime.now()
 
     str_time = time.strftime("%Y%m%d%H%M%S")
-    local( 'sudo mkdir -p versions')
+    local('sudo mkdir -p versions')
 
-    local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(str_time))
+    local("sudo tar -cvzf versions/web_static_{}.tgz web_static"
+          .format(str_time))
 
-    path_to_file = "versions/web_static_{}.tgz".format(str_time)
+    path_file = "versions/web_static_{}.tgz"\
+        .format(str_time)
     size_of_file = os.path.getsize(path_to_file)
 
-    print("web_static packed: {} -> {}Bytes".format(path_to_file, size_of_file))
+    print("web_static packed: {} -> {}Bytes".format(path_file, size_of_file))
 
     return path_to_file
