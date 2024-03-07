ccc866b9ac07dbbcbe8cdd535a738669406a73ab
Eric Ogbekene <ericogbekene@icloud.com>
2024-03-07 12:32:07 -0800

    web static deploy script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,10 +12,16 @@ sudo mkdir -p /data/web_static/shared
 
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/releases/test/index.html
-echo "Hello there old friend" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
 #create symbolic link
-sudo ln -T /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sTf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 sudo service nginx restart
@@ -26,7 +32,7 @@ sudo service nginx restart
 
 NGINX_CONFIG_FILE="/etc/nginx/sites-available/default"
 STATIC_CONTENT_PATH="/data/web_static/current/"
-DOMAIN_NAME="mydomainname.tech"
+DOMAIN_NAME="ericogbekene.tech"
 
 if ! grep -q "location /hbnb_static" "$NGINX_CONFIG_FILE"; then
 	sudo sed -i '/server_name '"$DOMAIN_NAME"';/a \        location /hbnb_static {\n            alias '"$STATIC_CONTENT_PATH"';\n        }' "$NGINX_CONFIG_FILE"
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,6 +5,7 @@ script to pack an archive using fabric
 
 import datetime
 from fabric.api import *
+import os
 
 def do_pack():
     """
@@ -18,3 +19,9 @@ def do_pack():
 
     local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(str_time))
 
+    path_to_file = "versions/web_static_{}.tgz".format(str_time))
+    size_of_file = os.path.getsize(path_to_file)
+
+    print("web_static packed: {} -> {}Bytes".format(path_to_file, size_of_file))
+
+    return path_to_file
