c0f3f5e9cf55fdc5c8174cf639e612fb7d675bda
Eric Ogbekene <ericogbekene@icloud.com>
2024-03-07 13:03:54 -0800

    script to create a tar archive record

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,14 +12,14 @@ def do_pack():
     function to pack a .tgz archive
     """
 
-    time = datetime.now()
+    time = datetime.datetime.now()
 
-    str_time = time.strftime(%Y%m%d%h%m%s)
-    local( 'sudo mkdir -p /versions')
+    str_time = time.strftime("%Y%m%d%H%M%S")
+    local( 'sudo mkdir -p versions')
 
     local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(str_time))
 
-    path_to_file = "versions/web_static_{}.tgz".format(str_time))
+    path_to_file = "versions/web_static_{}.tgz".format(str_time)
     size_of_file = os.path.getsize(path_to_file)
 
     print("web_static packed: {} -> {}Bytes".format(path_to_file, size_of_file))
