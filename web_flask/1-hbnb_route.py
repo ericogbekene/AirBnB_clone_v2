063ea38e4e7812f09bc0f3fe4502a16f4c90265f
Eric Ogbekene <ericogbekene@icloud.com>
2024-03-24 03:46:47 -0700

    setting up a new flask application with urls

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,5 +16,13 @@ def index():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb")
+def home():
+    """
+    mapping a url for /hbnb
+    """
+    return "HBNB"
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
