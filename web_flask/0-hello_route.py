2fe368802de0194a91679dd06065cdb97efab6e8
Eric Ogbekene <ericogbekene@icloud.com>
2024-03-24 02:07:09 -0700

    fixing @app.route properly to resolve 404 error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,16 +3,16 @@
 Creating flask app
 """
 
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/", strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index():
     """
     return index file
     """
-    return ("Hello HBNB")
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
